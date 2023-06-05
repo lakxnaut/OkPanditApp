cea90f0eba1ebdd444009f6c9c4594fa2fe82ebd
LAKSHYA\laksh <nautlaux@gmail.com>
2023-06-05 23:22:44 +0530

    2nd

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,21 @@
-import { View, Text, TextInput } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
 import { SIZES } from '../constants/theme'
 
 export default function LoginScreen() {
+    const [mobile, setMobile] = useState('')
     return (
         <View style={{ padding: 10, paddingTop: 50 }}>
             <Text style={{ fontSize: SIZES.h2, fontWeight: "900", color: 'black' }}>Enter Phone number</Text>
-            <TextInput style={{ backgroundColor: '#ebe6e6', marginTop: 10, borderRadius: 10 }} />
-            <Text style={{fontSize:SIZES.h4,margin:5}}>Please use your current number or your OkPandit
+            <TextInput 
+            onChangeText={setMobile}
+            value={mobile}
+            placeholder="Enter Mobile"
+            keyboardType="numeric"
+            style={{ backgroundColor: '#ebe6e6', marginTop: 10, borderRadius: 10 }} />
+            <Text style={{ fontSize: SIZES.h4, margin: 5, color: 'black' }}>Please use your current number or your OkPandit
                 registered number</Text>
+            <Button style={{ borderRadius: 20, padding: 110 }} title="Press me" />
         </View>
     )
 }
